078da1801109d5c86a7dc47718e0d1b8d586239c
Lucifer <158470225+lnnocent-Lucifer@users.noreply.github.com>
2026-06-11 16:19:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,90 +3,125 @@ const http = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
+
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: "*"
-  }
+    cors: {
+        origin: "*"
+    }
 });
 
 let onlineUsers = 0;
 
 io.on("connection", (socket) => {
 
-  console.log("User Connected");
+    console.log("User Connected");
 
-  onlineUsers++;
+    onlineUsers++;
 
-  io.emit("user_status", {
-    online: true,
-    count: onlineUsers
-  });
-
-  socket.on("send_message", (data) => {
-
-    socket.broadcast.emit(
-      "receive_message",
-      data
+    io.emit(
+        "user_status",
+        "online"
     );
 
-  });
+    socket.on(
+        "send_message",
+        (data) => {
 
-  socket.on("typing", () => {
+            socket.broadcast.emit(
+                "receive_message",
+                data
+            );
 
-    socket.broadcast.emit(
-      "typing"
+        }
     );
 
-  });
+    socket.on(
+        "typing",
+        () => {
 
-  socket.on("message_delivered", () => {
+            socket.broadcast.emit(
+                "typing"
+            );
 
-    socket.broadcast.emit(
-      "message_delivered"
+        }
     );
 
-  });
+    socket.on(
+        "message_delivered",
+        () => {
 
-  socket.on("message_seen", () => {
+            socket.broadcast.emit(
+                "message_delivered"
+            );
 
-    socket.broadcast.emit(
-      "message_seen"
+        }
     );
 
-  });
+    socket.on(
+        "message_seen",
+        () => {
 
-  socket.on("disconnect", () => {
+            socket.broadcast.emit(
+                "message_seen"
+            );
 
-    console.log("User Disconnected");
+        }
+    );
 
-    onlineUsers--;
+    socket.on(
+        "disconnect",
+        () => {
 
-    io.emit("user_status", {
-      online: onlineUsers > 0,
-      count: onlineUsers
-    });
+            console.log(
+                "User Disconnected"
+            );
 
-  });
+            onlineUsers--;
 
+            if (onlineUsers <= 0) {
+
+                onlineUsers = 0;
+
+                io.emit(
+                    "user_status",
+                    "offline"
+                );
+
+            } else {
+
+                io.emit(
+                    "user_status",
+                    "online"
+                );
+            }
+        }
+    );
 });
 
-app.get("/", (req, res) => {
+app.get(
+    "/",
+    (req, res) => {
 
-  res.send(
-    "Shreya Chat Server Running"
-  );
+        res.send(
+            "Shreya Chat Server Running"
+        );
 
-});
+    }
+);
 
 const PORT =
-  process.env.PORT || 3000;
+    process.env.PORT || 3000;
 
-server.listen(PORT, () => {
+server.listen(
+    PORT,
+    () => {
 
-  console.log(
-    "Server running on port " + PORT
-  );
+        console.log(
+            "Server running on port " +
+            PORT
+        );
 
-});
+    }
+);
