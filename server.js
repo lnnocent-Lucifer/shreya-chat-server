c5aef8658c53162f5b7f29f16b2e026501abbd8e
Lucifer <158470225+lnnocent-Lucifer@users.noreply.github.com>
2026-06-11 15:21:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,29 +11,82 @@ const io = new Server(server, {
   }
 });
 
+let onlineUsers = 0;
+
 io.on("connection", (socket) => {
 
   console.log("User Connected");
 
+  onlineUsers++;
+
+  io.emit("user_status", {
+    online: true,
+    count: onlineUsers
+  });
+
   socket.on("send_message", (data) => {
+
     socket.broadcast.emit(
-        "receive_message",
-        data
+      "receive_message",
+      data
     );
-});
+
+  });
+
+  socket.on("typing", () => {
+
+    socket.broadcast.emit(
+      "typing"
+    );
+
+  });
+
+  socket.on("message_delivered", () => {
+
+    socket.broadcast.emit(
+      "message_delivered"
+    );
+
+  });
+
+  socket.on("message_seen", () => {
+
+    socket.broadcast.emit(
+      "message_seen"
+    );
+
+  });
 
   socket.on("disconnect", () => {
+
     console.log("User Disconnected");
+
+    onlineUsers--;
+
+    io.emit("user_status", {
+      online: onlineUsers > 0,
+      count: onlineUsers
+    });
+
   });
 
 });
 
 app.get("/", (req, res) => {
-  res.send("Shreya Chat Server Running");
+
+  res.send(
+    "Shreya Chat Server Running"
+  );
+
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT =
+  process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+
+  console.log(
+    "Server running on port " + PORT
+  );
+
 });
