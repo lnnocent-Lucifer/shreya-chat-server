f441a2c3f715c46c84d6a1338cefcfcf569129c8
Lucifer <158470225+lnnocent-Lucifer@users.noreply.github.com>
2026-06-11 14:36:41 +0530

    Change message emission to broadcast instead of emit

--- server.js
+++ server.js
@@ -16,8 +16,11 @@ io.on("connection", (socket) => {
   console.log("User Connected");
 
   socket.on("send_message", (data) => {
-    io.emit("receive_message", data);
-  });
+    socket.broadcast.emit(
+        "receive_message",
+        data
+    );
+});
 
   socket.on("disconnect", () => {
     console.log("User Disconnected");
